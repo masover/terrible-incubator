ebef45b33b2cea646f5ea96df00909b1255ab242
David Masover <dave@forkbox.net>
2023-01-29 20:14:54 -0800

    Generate problems and one solution.
    This also introduces the state machine as, basically,
    applying one class to the whole document
    and showing/hiding everything else with CSS

--- main.js
+++ main.js
@@ -1,22 +1,48 @@
 import {problems} from './data/problems.js';
 import {pickRandom} from './util.js';
+import {generateSolution} from './solutions.js';
+
+function addToEach(elements, event, listener) {
+  for (const el of elements) {
+    el.addEventListener(event, listener);
+  }
+}
 
 class Page {
   constructor() {
-    this.problemDiv = document.getElementById('problem');
+    this.problemEl = document.getElementById('problem');
     this.problemControls = document.getElementById('problem-controls');
+    this.solutionEl = document.getElementById('solution');
+    this.previousSolutions = new Set();
   }
 
   setProblem() {
-    this.problemDiv.innerText = pickRandom(problems);
+    this.problem = pickRandom(problems)
+    this.problemEl.innerText = this.problem;
   }
 
   init() {
     this.setProblem();
-    const spinners = this.problemControls.getElementsByClassName('spin');
-    for (const spinner of spinners) {
-      spinner.addEventListener('click', () => this.setProblem());
+    addToEach(
+      this.problemControls.getElementsByClassName('spin'),
+      'click', () => this.setProblem());
+    addToEach(
+      this.problemControls.getElementsByClassName('ok'),
+      'click', () => this.acceptProblem());
+  }
+
+  proposeSolution() {
+    this.solution = generateSolution();
+    while (this.previousSolutions.has(solution)) {
+      this.solution = generateSolution();
     }
+    this.previousSolutions.add(this.solution);
+    this.solutionEl.innerText = this.solution;
+  }
+
+  acceptProblem() {
+    document.body.classList.add('problem-is-set');
+    this.proposeSolution();
   }
 }
 
